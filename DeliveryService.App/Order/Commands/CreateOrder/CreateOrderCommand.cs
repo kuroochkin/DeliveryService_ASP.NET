2b0ffeb02c7a075dd6a9632ee04405a949948237
pin11kurochkin@gmail.com <pin11kurochkin@gmail.com>
2023-03-21 18:58:55 +0300

    Логика добавления заказа

---
+++ DeliveryService.App/Order/Commands/CreateOrder/CreateOrderCommand.cs
@@ -0,0 +1,9 @@
+﻿using ErrorOr;
+using MediatR;
+
+namespace DeliveryService.App.Courier.Commands.AddCourier.AddOrder
+{
+	public record CreateOrderCommand(
+		string CourierId,
+		string CustomerId) : IRequest<ErrorOr<bool>>;
+}
---
+++ DeliveryService.App/Order/Commands/CreateOrder/CreateOrderCommandHandler.cs
@@ -0,0 +1,52 @@
+﻿using DeliveryService.App.Common.Errors;
+using DeliveryService.App.Common.Interfaces.Persistence;
+using DeliveryService.Domain.Order;
+using ErrorOr;
+using MediatR;
+
+namespace DeliveryService.App.Courier.Commands.AddCourier.AddOrder;
+
+public class CreateOrderCommandHandler
+	: IRequestHandler<CreateOrderCommand, ErrorOr<bool>>
+{
+	private readonly IUnitOfWork _unitOfWork;
+
+	public async Task<ErrorOr<bool>> Handle(
+		CreateOrderCommand request, 
+		CancellationToken cancellationToken)
+	{
+		if (!Guid.TryParse(request.CourierId, out var courierId))
+		{
+			return Errors.Courier.InvalidId;
+		}
+
+		if (!Guid.TryParse(request.CustomerId, out var customerId))
+		{
+			return Errors.Customer.InvalidId;
+		}
+
+		var courier = await _unitOfWork.Couriers.FindById(courierId);
+		if(courier is null)
+		{
+			return Errors.Courier.NotFound;
+		}
+
+		var customer = await _unitOfWork.Customers.FindById(customerId);
+		if (customer is null)
+		{
+			return Errors.Customer.NotFound;
+		}
+
+		var order = new OrderEntity(courier, customer);
+
+		if(await _unitOfWork.Orders.Add(order))
+		{
+			courier.AddOrder(order);
+			customer.AddOrder(order);
+
+			return await _unitOfWork.CompleteAsync();
+		}
+
+		return false;
+	}
+}
