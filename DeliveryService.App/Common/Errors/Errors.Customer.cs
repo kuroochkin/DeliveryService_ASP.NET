9f079fefc60c128803fd715c18247cd5378b679a
pin11kurochkin@gmail.com <pin11kurochkin@gmail.com>
2023-03-21 18:04:42 +0300

    Реализация обработки ошибок

---
+++ DeliveryService.App/Common/Errors/Errors.Customer.cs
@@ -0,0 +1,22 @@
+﻿using ErrorOr;
+
+namespace DeliveryService.App.Common.Errors;
+
+public static partial class Errors
+{
+	public static class Customer
+	{
+		public static Error InvalidId => Error.Validation(
+			code: "Customer.InvalidId",
+			description: "Id заказчика не найден.");
+
+		public static Error NotFound => Error.NotFound(
+			code: "Customer.NotFound",
+			description: "Заказчик не найден.");
+
+		public static Error CouldNotSave => Error.Conflict(
+			code: "Customer.CouldNotSave",
+			description: "Заказчик не был сохранен.");
+	}
+}
+
---
+++ DeliveryService.App/Common/Errors/Errors.Order.cs
@@ -0,0 +1,21 @@
+﻿using ErrorOr;
+
+namespace DeliveryService.App.Common.Errors;
+
+public static partial class Errors
+{
+	public static class Order
+	{
+		public static Error InvalidId => Error.Validation(
+			code: "Order.InvalidId",
+			description: "Id заказа не найден.");
+
+		public static Error NotFound => Error.NotFound(
+			code: "Order.NotFound",
+			description: "Заказ не найден.");
+
+		public static Error CouldNotSave => Error.Conflict(
+			code: "Order.CouldNotSave",
+			description: "Заказ не был сохранен.");
+	}
+}
