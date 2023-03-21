fe2fe76e42ad847c5acbc8742d0dd60c68f6d359
pin11kurochkin@gmail.com <pin11kurochkin@gmail.com>
2023-03-21 17:41:06 +0300

    Создание обобщенного управления репозиториями - UintOfWork

---
+++ DeliveryService.App/Common/Interfaces/Persistence/IUnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿namespace DeliveryService.App.Common.Interfaces.Persistence;
+
+public interface IUnitOfWork
+{
+	ICourierRepository Couriers { get; }
+	ICustomerRepository Customers { get; }
+	IOrderRepository Orders { get; }
+	IProductRepository Products { get; }
+
+	Task<bool> CompleteAsync();
+}
