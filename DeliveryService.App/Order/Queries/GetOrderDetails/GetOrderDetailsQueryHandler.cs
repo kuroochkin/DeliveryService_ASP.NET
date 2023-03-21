d574fbd8b28d37f15916adb4d0d99a07eff22eda
pin11kurochkin@gmail.com <pin11kurochkin@gmail.com>
2023-03-21 22:30:04 +0300

    Получение заказа (Query)

---
+++ DeliveryService.App/Order/Queries/GetOrderDetails/GetOrderDetailsQueryHandler.cs
@@ -0,0 +1,48 @@
+﻿using DeliveryService.App.Common.Errors;
+using DeliveryService.App.Common.Interfaces.Persistence;
+using ErrorOr;
+using MediatR;
+
+namespace DeliveryService.App.Order.Queries.GetOrderDetails;
+
+public class GetOrderDetailsQueryHandler
+	: IRequestHandler<GetOrderDetailsQuery, ErrorOr<OrderDetailsVm>>
+{
+	private readonly IUnitOfWork _unitOfWork;
+
+	public GetOrderDetailsQueryHandler(IUnitOfWork unitOfWork)
+	{
+		_unitOfWork = unitOfWork;
+	}
+
+	public async Task<ErrorOr<OrderDetailsVm>> Handle
+		(GetOrderDetailsQuery request, 
+		CancellationToken cancellationToken)
+	{
+		if (!Guid.TryParse(request.OrderId, out var orderId)) 
+		{
+			return Errors.Order.InvalidId;
+		}
+
+		var order = await _unitOfWork.Orders.FindById(orderId);
+		if (order is null)
+		{
+			return Errors.Order.NotFound;
+		}
+
+		var orderInfo = new OrderDetailsVm(
+			order.Id.ToString(),
+			order.Description,
+			order.Created,
+			new CourierVm(
+				order.Courier.Id.ToString(),
+				order.Courier.LastName,
+				order.Courier.FirstName),
+			new CustomerVm(
+				order.Courier.Id.ToString(),
+				order.Courier.LastName,
+				order.Courier.FirstName));
+
+		return orderInfo;	
+	}
+}
