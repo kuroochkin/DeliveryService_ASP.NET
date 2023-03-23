0fbe2e9f862b3d9be2dda673c8cc2d09b2c7a2ba
pin11kurochkin@gmail.com <pin11kurochkin@gmail.com>
2023-03-23 13:57:45 +0300

    Составляем контейнер, подключаем зависимости

--- DeliveryService_ASP.NET/Program.cs
+++ DeliveryService_ASP.NET/Program.cs
@@ -1,13 +1,39 @@
+using DeliveryService.API;
+using DeliveryService.App;
 using DeliveryService.infrastructure;
+using Microsoft.AspNetCore.Builder;
 
 var builder = WebApplication.CreateBuilder(args);
 {
 	builder.Services
-		.AddInfrastructure(builder.Configuration);
+		.AddApplication()
+		.AddInfrastructure(builder.Configuration)
+		.AddPresentation();
+
+	builder.Services
+		.AddCors(options =>
+		{
+			options.AddPolicy("AllowAllHeaders", builder =>
+			{
+				builder.AllowAnyOrigin()
+					   .AllowAnyHeader()
+					   .AllowAnyMethod();
+			});
+		});
 }
 
 var app = builder.Build();
 {
+	if (app.Environment.IsDevelopment())
+	{
+		app.UseSwagger();
+		app.UseSwaggerUI();
+	}
+
+
+	app.UseCors("AllowAllHeaders");
+
+	app.MapControllers();
 
 	app.Run();
 }
