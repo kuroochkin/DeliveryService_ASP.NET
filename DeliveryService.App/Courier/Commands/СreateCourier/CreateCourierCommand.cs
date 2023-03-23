2dc51cc20fd1036cc0ca6dcfe853c330def84179
pin11kurochkin@gmail.com <pin11kurochkin@gmail.com>
2023-03-23 13:54:47 +0300

    Обработчик создания заказчика

---
+++ DeliveryService.App/Courier/Commands/СreateCourier/CreateCourierCommand.cs
@@ -0,0 +1,9 @@
+﻿using ErrorOr;
+using MediatR;
+
+namespace DeliveryService.App.Courier.Commands.СreateCourier;
+
+public record СreateCourierCommand(
+	string LastName,
+	string FirstName,
+	string Patromymic) : IRequest<ErrorOr<bool>>;
