c9e466ca39799204234f0cbe88fb6828fee3c6c2
pin11kurochkin@gmail.com <pin11kurochkin@gmail.com>
2023-03-23 13:55:55 +0300

    Создание контроллеров WebApi

---
+++ DeliveryService_ASP.NET/Controllers/CustomerController.cs
@@ -0,0 +1,34 @@
+﻿using DeliveryService.App.Customer.Commands.CreateCustomer;
+using DeliveryService.Contracts.Customer;
+using MapsterMapper;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DeliveryService.API.Controllers
+{
+	[Route("api/customer")]
+	public class CustomerController : Controller
+	{
+		private readonly ISender _mediator;
+		private readonly IMapper _mapper;
+
+		public CustomerController(ISender mediator, IMapper mapper)
+		{
+			_mediator = mediator;
+			_mapper = mapper;
+		}
+
+		[HttpPost("create")]
+		public async Task<IActionResult> CreateCustomer(CreateCustomerRequest request)
+		{
+			var command = _mapper.Map<СreateCustomerCommand>(request);
+
+			var result = await _mediator.Send(command);
+
+			return result.Match(
+				coursesResult => Ok(result.Value),
+				errors => Problem("Ошибка")
+				);
+		}
+	}
+}
---
+++ DeliveryService_ASP.NET/Controllers/OrderController.cs
@@ -0,0 +1,35 @@
+﻿using MapsterMapper;
+using DeliveryService.App.Courier.Commands.AddCourier.AddOrder;
+using DeliveryService.Contracts.Order;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace DeliveryService.API.Controllers
+{
+	[Route("api/order")]
+	public class OrderController : Controller
+	{
+		private readonly ISender _mediator;
+		private readonly IMapper _mapper;
+
+		public OrderController(ISender mediator, IMapper mapper)
+		{
+			_mediator = mediator;
+			_mapper = mapper;
+		}
+
+		[HttpPost("create")]
+		public async Task<IActionResult> CreateOrder(CreateOrderRequest request)
+		{
+			var command = _mapper.Map<CreateOrderCommand>(request);
+
+			var result = await _mediator.Send(command);
+
+			return result.Match(
+				coursesResult => Ok(result.Value),
+				errors => Problem("Ошибка")
+				);
+		}
+	}
+}
