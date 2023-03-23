f8405da4d189f61ae9117a1b4a1426fee50843a1
pin11kurochkin@gmail.com <pin11kurochkin@gmail.com>
2023-03-23 13:55:34 +0300

    Новая система маппинга

---
+++ DeliveryService_ASP.NET/Common/Mapping/CourierMappingConfig.cs
@@ -0,0 +1,16 @@
+﻿using DeliveryService.App.Courier.Commands.СreateCourier;
+using DeliveryService.Contracts.Courier;
+using Mapster;
+
+namespace DeliveryService.API.Common.Mapping;
+
+public class CourierMappingConfig : IRegister
+{
+	public void Register(TypeAdapterConfig config)
+	{
+		config.NewConfig<CreateCourierRequest, СreateCourierCommand>()
+			.Map(dest => dest.LastName, src => src.LastName)
+			.Map(dest => dest.FirstName, src => src.FirstName)
+			.Map(dest => dest.Patromymic, src => src.Patronymic);
+	}
+}
---
+++ DeliveryService_ASP.NET/Common/Mapping/OrderMappingConfig.cs
@@ -0,0 +1,21 @@
+﻿using DeliveryService.App.Courier.Commands.AddCourier.AddOrder;
+using DeliveryService.App.Order.Queries.GetOrderDetails;
+using DeliveryService.Contracts.Order;
+using DeliveryService.Contracts.Order.Get;
+using Mapster;
+
+namespace DeliveryService.API.Common.Mapping
+{
+	public class OrderMappingConfig : IRegister
+	{
+		public void Register(TypeAdapterConfig config)
+		{
+			config.NewConfig<OrderDetailsVm, GetOrderDetailsResponse>();
+
+			config.NewConfig<CreateOrderRequest, CreateOrderCommand>()
+				.Map(dest => dest.CourierId, src => src.CourierId)
+				.Map(dest => dest.CustomerId, src => src.CustomerId)
+				.Map(dest => dest.Description, src => src.Description);
+		}
+	}
+}
