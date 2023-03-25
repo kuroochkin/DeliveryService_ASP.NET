bf9d04685b0ef0a343f526f6177fc0214ee3b446
pin11kurochkin@gmail.com <pin11kurochkin@gmail.com>
2023-03-25 17:08:43 +0300

    Репозиторий пользователя

---
+++ DeliveryService.App/Common/Interfaces/Persistence/IUserRepository.cs
@@ -0,0 +1,7 @@
+﻿using DeliveryService.Domain.User;
+
+namespace DeliveryService.App.Common.Interfaces.Persistence;
+
+public interface IUserRepository : IGenericRepository<UserEntity>
+{
+}
---
+++ DeliveryService.infrastructure/Persistence/Repositories/UserRepository.cs
@@ -0,0 +1,12 @@
+﻿using DeliveryService.App.Common.Interfaces.Persistence;
+using DeliveryService.Domain.Customer;
+using DeliveryService.Domain.User;
+
+namespace DeliveryService.infrastructure.Persistence.Repositories;
+
+public class UserRepository : GenericRepository<UserEntity>, IUserRepository
+{
+	public UserRepository(ApplicationDbContext context) : base(context)
+	{
+	}
+}
