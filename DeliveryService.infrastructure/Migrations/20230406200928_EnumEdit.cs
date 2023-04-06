4ece6d046d965521230fd3fad0f9536014595d66
pin11kurochkin@gmail.com <pin11kurochkin@gmail.com>
2023-04-06 23:14:05 +0300

    Миграции для EnumUser

---
+++ DeliveryService.infrastructure/Migrations/20230406200928_EnumEdit.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DeliveryService.infrastructure.Migrations
+{
+    public partial class EnumEdit : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+			migrationBuilder.DropColumn(
+					name: "UserType",
+					table: "Users");
+
+			migrationBuilder.DropColumn(
+					name: "GetUserTypeToString",
+					table: "Users");
+		}
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
