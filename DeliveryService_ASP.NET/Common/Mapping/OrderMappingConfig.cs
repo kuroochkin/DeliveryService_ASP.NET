5c156bfaf4f9a36a1a9c21a0288cd0a965d1e9a2
pin11kurochkin@gmail.com <pin11kurochkin@gmail.com>
2023-06-03 18:28:55 +0300

    Маппинг

--- DeliveryService_ASP.NET/Common/Mapping/OrderMappingConfig.cs
+++ DeliveryService_ASP.NET/Common/Mapping/OrderMappingConfig.cs
@@ -7,33 +7,32 @@ using DeliveryService.Contracts.Order.Get;
 using Mapster;
 using Microsoft.EntityFrameworkCore.Design;
 
-namespace DeliveryService.API.Common.Mapping
+namespace DeliveryService.API.Common.Mapping;
+
+public class OrderMappingConfig : IRegister
 {
-	public class OrderMappingConfig : IRegister
+	public void Register(TypeAdapterConfig config)
 	{
-		public void Register(TypeAdapterConfig config)
-		{
-			config.NewConfig<OrderDetailsVm, GetOrderDetailsResponse>()
-				.Map(dest => dest.OrderId, src => src.OrderId)
-				.Map(dest => dest.Courier, src => src.Courier)
-				.Map(dest => dest.Customer, src => src.Customer)
-				.Map(dest => dest.Created, src => src.Created)
-				.Map(dest => dest.Status, src => src.Status)
-				.Map(dest => dest.Description, src => src.Description);
+		config.NewConfig<OrderDetailsVm, GetOrderDetailsResponse>()
+			.Map(dest => dest.OrderId, src => src.OrderId)
+			.Map(dest => dest.Courier, src => src.Courier)
+			.Map(dest => dest.Customer, src => src.Customer)
+			.Map(dest => dest.Created, src => src.Created)
+			.Map(dest => dest.Status, src => src.Status)
+			.Map(dest => dest.Description, src => src.Description);
 
-			config.NewConfig<(CreateOrderRequest request, string customerId), CreateOrderCommand>()
-				.Map(dest => dest.CustomerId, src => src.customerId)
-				.Map(dest => dest.Description, src => src.request.Description);
+		config.NewConfig<(CreateOrderRequest request, string customerId), CreateOrderCommand>()
+			.Map(dest => dest.CustomerId, src => src.customerId)
+			.Map(dest => dest.Description, src => src.request.Description);
 
-			config.NewConfig<(ConfirmOrderRequest request, string courierId), ConfirmOrderCommand>()
-				.Map(dest => dest.CourierId, src => src.courierId)
-				.Map(dest => dest.OrderId, src => src.request.OrderId);
+		config.NewConfig<(ConfirmOrderRequest request, string courierId), ConfirmOrderCommand>()
+			.Map(dest => dest.CourierId, src => src.courierId)
+			.Map(dest => dest.OrderId, src => src.request.OrderId);
 
-			config.NewConfig<OrdersUserVm, GetOrdersCustomerResponse>()
-				.Map(dest => dest.Orders, src => src.Orders);
+		config.NewConfig<OrdersUserVm, GetOrdersCustomerResponse>()
+			.Map(dest => dest.Orders, src => src.Orders);
 
-			config.NewConfig<OrdersUserVm, GetOrdersCourierResponse>()
-				.Map(dest => dest.Orders, src => src.Orders);
-		}
+		config.NewConfig<OrdersUserVm, GetOrdersCourierResponse>()
+			.Map(dest => dest.Orders, src => src.Orders);
 	}
 }
