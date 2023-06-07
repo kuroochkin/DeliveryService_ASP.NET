3661d5ba69d98ea759da9268673445fcd230e03e
pin11kurochkin@gmail.com <pin11kurochkin@gmail.com>
2023-06-07 22:51:47 +0300

    Новые конфигурации

---
+++ DeliveryService.infrastructure/Persistence/EntityTypeConfiguration/OrderItemConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using DeliveryService.Domain.Courier;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using DeliveryService.Domain;
+
+namespace DeliveryService.infrastructure.Persistence.EntityTypeConfiguration;
+
+public class OrderItemConfiguration: IEntityTypeConfiguration<OrderItemEntity>
+{
+	public void Configure(EntityTypeBuilder<OrderItemEntity> builder)
+	{
+		builder.ToTable("OrderItem");
+		builder.HasKey(item => item.Id);
+
+		builder.Property(item => item.Count);
+		builder.Property(item => item.TotalPrice);
+
+	}
+}
