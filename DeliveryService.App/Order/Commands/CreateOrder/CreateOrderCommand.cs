509df02b67caccde9606688c01520b988aab1e8e
pin11kurochkin@gmail.com <pin11kurochkin@gmail.com>
2023-06-07 22:50:54 +0300

    Новая логика принятия заказа

--- DeliveryService.App/Order/Commands/CreateOrder/CreateOrderCommand.cs
+++ DeliveryService.App/Order/Commands/CreateOrder/CreateOrderCommand.cs
@@ -1,9 +1,12 @@
-﻿using ErrorOr;
+﻿using DeliveryService.Contracts.Order;
+using DeliveryService.Domain.Product;
+using ErrorOr;
 using MediatR;
 
 namespace DeliveryService.App.Courier.Commands.AddCourier.AddOrder
 {
 	public record CreateOrderCommand(
 		string CustomerId,
-		string Description) : IRequest<ErrorOr<bool>>;
+		string Description,
+		List<GetProductRequest> Products) : IRequest<ErrorOr<bool>>;
 }
