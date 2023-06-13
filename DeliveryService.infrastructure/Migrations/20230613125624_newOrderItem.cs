838045e83962da861f5da79d6697078cb8148ffa
pin11kurochkin@gmail.com <pin11kurochkin@gmail.com>
2023-06-13 17:49:27 +0300

    Новые миграции

---
+++ DeliveryService.infrastructure/Migrations/20230613125624_newOrderItem.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DeliveryService.infrastructure.Migrations
+{
+    public partial class newOrderItem : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Thumbnail",
+                table: "OrderItem",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Title",
+                table: "OrderItem",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Thumbnail",
+                table: "OrderItem");
+
+            migrationBuilder.DropColumn(
+                name: "Title",
+                table: "OrderItem");
+        }
+    }
+}
