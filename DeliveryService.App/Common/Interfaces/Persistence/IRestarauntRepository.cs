ea1e6e1dd12d54a5326e387ebd9b96361b3d1cf2
pin11kurochkin@gmail.com <pin11kurochkin@gmail.com>
2023-09-11 23:31:50 +0300

    Добавление интерфейса репозитория ресторана

---
+++ DeliveryService.App/Common/Interfaces/Persistence/IRestarauntRepository.cs
@@ -0,0 +1,8 @@
+﻿using DeliveryService.Domain.Restaraunt;
+
+namespace DeliveryService.App.Common.Interfaces.Persistence;
+
+public interface IRestarauntRepository : IGenericRepository<RestarauntEntity>
+{
+
+}
--- DeliveryService.Domain/Restaraunt/RestarauntEntity.cs
+++ DeliveryService.Domain/Restaraunt/RestarauntEntity.cs
@@ -1,12 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using static DeliveryService.Domain.Order.OrderEntity;
 
-namespace DeliveryService.Domain.Restaraunt
+namespace DeliveryService.Domain.Restaraunt;
+
+public class RestarauntEntity
 {
-	internal class RestarauntEntity
+	public Guid Id { get; set; }
+
+	public string Name { get; set; }
+
+	public string? Address { get; set; }
+
+	public IsOpen Status { get; set; }
+
+	public IsOpen GetStatus => Status;
+
+	public string GetStatusIsOpenToString()
 	{
+		switch (Status)
+		{
+			case IsOpen.Close:
+				return "Close";
+			case IsOpen.Open:
+				return "Open";
+		}
+
+		return "";
 	}
+
+	public enum IsOpen
+	{
+		Close,
+		Open
+	};
 }
