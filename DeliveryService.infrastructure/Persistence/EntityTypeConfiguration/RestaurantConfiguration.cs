6c5d92d935ccbc1d58b0bc56ffbeb0aed5200d6d
pin11kurochkin@gmail.com <pin11kurochkin@gmail.com>
2023-09-11 23:32:27 +0300

    Конфигурация для сущности ресторана

---
+++ DeliveryService.infrastructure/Persistence/EntityTypeConfiguration/RestaurantConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using DeliveryService.Domain.Restaraunt;
+
+namespace DeliveryService.infrastructure.Persistence.EntityTypeConfiguration;
+
+public class RestaurantConfiguration : IEntityTypeConfiguration<RestarauntEntity>
+{
+	public void Configure(EntityTypeBuilder<RestarauntEntity> builder)
+	{
+		builder.ToTable("Restaraunts");
+		builder.HasKey(restaraunt => restaraunt.Id);
+		builder.Property(restaraunt => restaraunt.Name);
+		builder.Property(restaraunt => restaraunt.Address);
+		builder.Property(restaraunt => restaraunt.Status);
+
+	}
+}
