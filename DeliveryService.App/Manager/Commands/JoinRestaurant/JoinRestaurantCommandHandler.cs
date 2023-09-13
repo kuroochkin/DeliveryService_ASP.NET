dff05f0ce57e2c5248c383f632d0afa6984c263d
pin11kurochkin@gmail.com <pin11kurochkin@gmail.com>
2023-09-13 17:53:22 +0300

    Запрос на привязку ресторана к менеджеру

---
+++ DeliveryService.App/Manager/Commands/JoinRestaurant/JoinRestaurantCommandHandler.cs
@@ -0,0 +1,48 @@
+﻿using DeliveryService.App.Common.Errors;
+using DeliveryService.App.Common.Interfaces.Persistence;
+using ErrorOr;
+using MediatR;
+
+namespace DeliveryService.App.Manager.Commands.JoinRestaurant;
+
+public class JoinRestaurantCommandHandler
+	: IRequestHandler<JoinRestaurantCommand, ErrorOr<bool>>
+{
+	private readonly IUnitOfWork _unitOfWork;
+
+	public JoinRestaurantCommandHandler(IUnitOfWork unitOfWork)
+	{
+		_unitOfWork = unitOfWork;
+	}
+	public async Task<ErrorOr<bool>> Handle(
+		JoinRestaurantCommand request, 
+		CancellationToken cancellationToken)
+	{
+		if (!Guid.TryParse(request.ManagerId, out var managerId))
+		{
+			return Errors.Manager.InvalidId;
+		}
+
+		if (!Guid.TryParse(request.RestaurantId, out var restaurantId))
+		{
+			return Errors.Restaurant.InvalidId;
+		}
+
+		var restaurant = await _unitOfWork.Restaurants.FindById(restaurantId);
+		if (restaurant is null)
+		{
+			return Errors.Restaurant.NotFound;
+		}
+
+		var manager = await _unitOfWork.Managers.FindById(managerId);
+		if (manager is null)
+		{
+			return Errors.Manager.NotFound;
+		}
+
+		// Привязка менеджера к конкретному ресторану
+		manager.Restaurant = restaurant;
+
+		return await _unitOfWork.CompleteAsync();
+	}
+}
---
+++ DeliveryService_ASP.NET/Controllers/ManagerController.cs
@@ -0,0 +1,39 @@
+﻿using DeliveryService.App.Manager.Commands.JoinRestaurant;
+using DeliveryService.Contracts.Manager;
+using MapsterMapper;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace DeliveryService.API.Controllers;
+
+[ApiController]
+[Route("api/manager")]
+public class ManagerController : ApiController
+{
+	private readonly ISender _mediator;
+	private readonly IMapper _mapper;
+
+	public ManagerController(ISender mediator, IMapper mapper)
+	{
+		_mediator = mediator;
+		_mapper = mapper;
+	}
+
+	[HttpPost("addRestaurant")]
+	[Authorize(Roles = "Manager")]
+	public async Task<IActionResult> AddRestaurant(JoinRestaurantRequest request)
+	{
+		var managerId = GetUserId();
+
+		var command = _mapper.Map<JoinRestaurantCommand>((request, managerId));
+
+		var result = await _mediator.Send(command);
+
+		return result.Match(
+			Result => Ok(result.Value),
+			errors => Problem("Ошибка")
+			);
+	}
+}
