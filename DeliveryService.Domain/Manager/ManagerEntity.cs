74a6c0a69f59b57826fd14fa3892050fefd9e046
pin11kurochkin@gmail.com <pin11kurochkin@gmail.com>
2023-09-13 16:15:34 +0300

    Исправлен баг с созданием менеджера

--- DeliveryService.Domain/Manager/ManagerEntity.cs
+++ DeliveryService.Domain/Manager/ManagerEntity.cs
@@ -6,7 +6,7 @@ namespace DeliveryService.Domain.Manager;
 public class ManagerEntity
 {
 	public Guid Id { get; set; }
-	public RestaurantEntity Restaurant { get; set; }
+	public RestaurantEntity? Restaurant { get; set; }
 	public string LastName { get; set; }
 	public string FirstName { get; set; }
 	public int CountOrder { get; set; }
