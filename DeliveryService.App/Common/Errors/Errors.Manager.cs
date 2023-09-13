c3425dd47f6ffb6a9a64917b7f976620abb8292f
pin11kurochkin@gmail.com <pin11kurochkin@gmail.com>
2023-09-13 16:31:21 +0300

    Прописаны Errors для менеджера

---
+++ DeliveryService.App/Common/Errors/Errors.Manager.cs
@@ -0,0 +1,21 @@
+﻿using ErrorOr;
+
+namespace DeliveryService.App.Common.Errors;
+
+public static partial class Errors
+{
+	public static class Manager
+	{
+		public static Error InvalidId => Error.Validation(
+			code: "Manager.InvalidId",
+			description: "Id менеджера не найден.");
+
+		public static Error NotFound => Error.NotFound(
+			code: "Manager.NotFound",
+			description: "Менеджер не найден.");
+
+		public static Error CouldNotSave => Error.Conflict(
+			code: "Manager.CouldNotSave",
+			description: "Менеджер не был сохранен.");
+	}
+}
