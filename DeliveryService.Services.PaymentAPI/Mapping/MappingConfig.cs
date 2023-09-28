103522882d23abcb7b226e7fc9512dedd2c85947
pin11kurochkin <pin11kurochkin@gmail.com>
2023-09-29 00:03:37 +0300

    Репозиторий оплаты

---
+++ DeliveryService.Services.PaymentAPI/Mapping/MappingConfig.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+
+namespace DeliveryService.Services.PaymentAPI.Mapping;
+
+public class MappingConfig
+{
+	public static MapperConfiguration RegisterMaps()
+	{
+		var mappingConfig = new MapperConfiguration(config =>
+		{
+
+		});
+
+		return mappingConfig;
+	}
+}
