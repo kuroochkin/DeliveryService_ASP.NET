561e18cdf076bf6637994c1f6c7872f17930cc67
pin11kurochkin <pin11kurochkin@gmail.com>
2023-10-10 17:03:01 +0300

    Обработка ошибок для сущностей ProductAPI

---
+++ DeliveryService.Services.ProductAPI.App/Common/Errors/Errors.Section.cs
@@ -0,0 +1,13 @@
+﻿using ErrorOr;
+
+namespace DeliveryService.Services.ProductAPI.App.Common.Errors;
+
+public static partial class Errors
+{
+	public static class Section
+	{
+		public static Error InvalidId => Error.Validation(
+			code: "Section.InvalidId",
+			description: "Id секции не найдено.");
+	}
+}
