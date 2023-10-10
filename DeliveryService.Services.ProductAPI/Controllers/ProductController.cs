59cda440eea89975507ebac68b54cd713bf02778
pin11kurochkin <pin11kurochkin@gmail.com>
2023-10-10 21:57:16 +0300

    Контроллеры продуктов и секций

---
+++ DeliveryService.Services.ProductAPI/Controllers/ProductController.cs
@@ -0,0 +1,62 @@
+﻿using DeliveryService.Services.ProductAPI.App.Product.Queries.GetAllProducts;
+using DeliveryService.Services.ProductAPI.App.Product.Queries.GetProductsBySection;
+using DeliveryService.Services.ProductAPI.App.Product.Queries.GetProductsDetails;
+using DeliveryService.Services.ProductAPI.Contracts.Product.Get;
+using MapsterMapper;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DeliveryService.Services.ProductAPI.Controllers;
+
+[ApiController]
+[Route("api/product")]
+public class ProductController : Controller
+{
+	private readonly ISender _mediator;
+	private readonly IMapper _mapper;
+
+	public ProductController(ISender mediator, IMapper mapper)
+	{
+		_mediator = mediator;
+		_mapper = mapper;
+	}
+
+	[HttpGet("allProducts")]
+	public async Task<IActionResult> GetAllProducts()
+	{
+		var query = new GetAllProductsQuery();
+
+		var productResult = await _mediator.Send(query);
+
+		return productResult.Match(
+			orders => Ok(_mapper.Map<GetAllProductsResponse>(orders)),
+			errors => Problem("Ошибка")
+		);
+	}
+
+	[HttpGet("allProducts/{sectionId}")]
+	public async Task<IActionResult> GetAllProductsBySection(string sectionId)
+	{
+		var query = new GetProductsBySectionQuery(sectionId);
+
+		var productResult = await _mediator.Send(query);
+
+		return productResult.Match(
+			orders => Ok(_mapper.Map<GetAllProductsResponse>(orders)),
+			errors => Problem("Ошибка")
+		);
+	}
+
+	[HttpGet("{productId}")]
+	public async Task<IActionResult> GetProductById(string productId)
+	{
+		var query = new GetProductsDetailsQuery(productId);
+
+		var productResult = await _mediator.Send(query);
+
+		return productResult.Match(
+			product => Ok(_mapper.Map<GetProductDetailsResponse>(product)),
+			errors => Problem("Ошибка")
+		);
+	}
+}
---
+++ DeliveryService.Services.ProductAPI/Controllers/SectionController.cs
@@ -0,0 +1,34 @@
+﻿
+using DeliveryService.Services.ProductAPI.App.Section.Queries.GetAllSections;
+using DeliveryService.Services.ProductAPI.Contracts.Section.Get;
+using MapsterMapper;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DeliveryService.Services.ProductAPI.Controllers;
+
+[Route("api/sections")]
+public class SectionController : Controller
+{
+	private readonly ISender _mediator;
+	private readonly IMapper _mapper;
+
+	public SectionController(ISender mediator, IMapper mapper)
+	{
+		_mediator = mediator;
+		_mapper = mapper;
+	}
+
+	[HttpGet("allSections")]
+	public async Task<IActionResult> GetAllSections()
+	{
+		var query = new GetAllSectionsQuery();
+
+		var productResult = await _mediator.Send(query);
+
+		return productResult.Match(
+			orders => Ok(_mapper.Map<GetAllSectionsResponse>(orders)),
+			errors => Problem("Ошибка")
+		);
+	}
+}
