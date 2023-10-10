6080bd60f3fe42e287b31cf84398a76a819cd325
pin11kurochkin <pin11kurochkin@gmail.com>
2023-10-10 18:00:16 +0300

    Контекст БД для ProductAPI

---
+++ DeliveryService.Services.ProductAPI.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using DeliveryService.Services.ProductAPI.Domain.Product;
+using DeliveryService.Services.ProductAPI.Domain.Section;
+using DeliveryService.Services.ProductAPI.Infrastructure.Persistence.EntityTypeConfiguration;
+using Microsoft.EntityFrameworkCore;
+
+namespace DeliveryService.Services.ProductAPI.Infrastructure.Persistence;
+
+public class ApplicationDbContext : DbContext
+{
+	public DbSet<ProductEntity> Products { get; set; }
+	public DbSet<SectionEntity> Sections { get; set; }
+
+	public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+	   : base(options) { }
+
+	protected override void OnModelCreating(ModelBuilder builder)
+	{
+		builder.ApplyConfiguration(new ProductConfiguration());
+		builder.ApplyConfiguration(new SectionConfiguration());
+
+		base.OnModelCreating(builder);
+	}
+}
