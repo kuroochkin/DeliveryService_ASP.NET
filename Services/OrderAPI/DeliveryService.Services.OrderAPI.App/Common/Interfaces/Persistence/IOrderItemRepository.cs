02bc503d362c5998277ea92187867b4882edc6c5
pin11kurochkin <pin11kurochkin@gmail.com>
2023-10-16 16:41:29 +0300

    Репозитории

---
+++ Services/OrderAPI/DeliveryService.Services.OrderAPI.App/Common/Interfaces/Persistence/IOrderItemRepository.cs
@@ -0,0 +1,7 @@
+﻿using DeliveryService.Services.OrderAPI.Domain.OrderItem;
+
+namespace DeliveryService.Services.OrderAPI.App.Common.Interfaces.Persistence;
+
+public interface IOrderItemRepository : IGenericRepository<OrderItemEntity>
+{
+}
---
+++ Services/OrderAPI/DeliveryService.Services.OrderAPI.App/Common/Interfaces/Persistence/IOrderRepository.cs
@@ -0,0 +1,23 @@
+﻿using DeliveryService.Services.OrderAPI.Domain.Order;
+using static DeliveryService.Services.OrderAPI.Domain.Order.OrderEntity;
+
+namespace DeliveryService.Services.OrderAPI.App.Common.Interfaces.Persistence;
+
+public interface IOrderRepository : IGenericRepository<OrderEntity>
+{
+	Task<OrderEntity?> FindOrderWithCustomer(Guid id);
+
+	Task<OrderEntity?> FindOrderWithCustomerAndManager(Guid id);
+
+	Task<OrderEntity?> FindOrderWithCustomerAndCourier(Guid id);
+
+	Task<OrderEntity?> FindOrderWithCustomerAndCourierAndManager(Guid id);
+
+	Task<List<OrderEntity>?> FindOrdersByCustomerId(Guid id);
+
+	Task<List<OrderEntity>?> FindOrdersByCourierId(Guid id);
+
+	Task<List<OrderEntity>?> FindOrdersByCustomerIdByOrderStatus(Guid id, OrderStatus orderStatus);
+
+	Task<List<OrderEntity>?> FindOrdersByCourierIdByOrderStatus(Guid id, OrderStatus orderStatus);
+}
