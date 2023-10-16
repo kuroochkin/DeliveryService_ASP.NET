825d28d52b9dc9ba4e0e6d7fd880770c00e28397
pin11kurochkin <pin11kurochkin@gmail.com>
2023-10-16 16:34:27 +0300

    Настрйока архитектуры

---
+++ Services/OrderAPI/DeliveryService.Services.OrderAPI.Domain/Order/OrderEntity.cs
@@ -0,0 +1,71 @@
+﻿using DeliveryService.Services.OrderAPI.Domain.OrderItem;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DeliveryService.Services.OrderAPI.Domain.Order;
+
+public class OrderEntity
+{
+	public Guid Id { get; }
+
+	#region Date
+	public DateTime Created { get; set; }
+
+	public DateTime ConfirmedRestaurant { get; set; }
+
+	public DateTime EndRestaurant { get; set; }
+
+	public DateTime ConfirmedCourier { get; set; }
+
+	public DateTime End { get; set; }
+
+	#endregion
+	public string Description { get; set; }
+
+	public bool PaymentStatus { get; set; }
+
+	public enum OrderStatus
+	{
+		Create,
+		ConfirmedRestaurant,
+		EndRestaurant,
+		ConfirmedCourier,
+		Complete,
+	};
+
+	[NotMapped]
+	public OrderStatus Status { get; set; }
+	public OrderStatus GetStatus => Status;
+
+	public string GetStatusOrderToString()
+	{
+		switch (Status)
+		{
+			case OrderStatus.Create:
+				return "Create";
+			case OrderStatus.ConfirmedRestaurant:
+				return "ConfirmedRestaurant";
+			case OrderStatus.EndRestaurant:
+				return "EndRestaurant";
+			case OrderStatus.ConfirmedCourier:
+				return "ConfirmedCourier";
+			case OrderStatus.Complete:
+				return "Complete";
+		}
+
+		return "";
+	}
+
+	public Guid CourierId { get; set; }
+
+	public Guid CustomerId { get; set; }
+
+	public Guid Manager { get; set; }
+
+	public List<OrderItemEntity> OrderItems { get; set; } = new();
+
+	public OrderEntity()
+	{
+		Id = Guid.NewGuid();
+		Created = DateTime.UtcNow;
+	}
+}
---
+++ Services/OrderAPI/DeliveryService.Services.OrderAPI.Domain/OrderItem/OrderItemEntity.cs
@@ -0,0 +1,33 @@
+﻿namespace DeliveryService.Services.OrderAPI.Domain.OrderItem;
+
+public class OrderItemEntity
+{
+	public Guid Id { get; set; }
+
+	public int Count { get; set; }
+
+	public double TotalPrice { get; set; }
+
+	public string Title { get; set; }
+
+	public string Thumbnail { get; set; }
+
+	public int ProductId { get; set; }
+
+	public Guid OrderId { get; set; }
+
+	public OrderItemEntity(
+		int count, 
+		double totalPrice, 
+		int productId, 
+		string thumbnail, 
+		string title)
+	{
+		Id = new Guid();
+		Count = count;
+		TotalPrice = totalPrice;
+		ProductId = productId;
+		Title = title;
+		Thumbnail = thumbnail;
+	}
+}
