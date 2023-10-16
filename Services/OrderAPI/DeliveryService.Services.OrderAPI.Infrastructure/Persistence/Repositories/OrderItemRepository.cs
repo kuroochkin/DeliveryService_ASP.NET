7974bde44e7b3ba7235e2f3e6af1078cde973520
pin11kurochkin <pin11kurochkin@gmail.com>
2023-10-16 18:30:01 +0300

    Реализация репозиториев

---
+++ Services/OrderAPI/DeliveryService.Services.OrderAPI.Infrastructure/Persistence/Repositories/OrderItemRepository.cs
@@ -0,0 +1,11 @@
+﻿using DeliveryService.Services.OrderAPI.App.Common.Interfaces.Persistence;
+using DeliveryService.Services.OrderAPI.Domain.OrderItem;
+
+namespace DeliveryService.Services.OrderAPI.Infrastructure.Persistence.Repositories;
+
+public class OrderItemRepository : GenericRepository<OrderItemEntity>, IOrderItemRepository
+{
+	public OrderItemRepository(ApplicationDbContext context) : base(context)
+	{
+	}
+}
