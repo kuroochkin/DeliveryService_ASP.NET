4a07f6fdffe7c6c0c91915068bf01cfe96c06efa
pin11kurochkin <pin11kurochkin@gmail.com>
2023-10-16 17:00:09 +0300

    Конфигурация сущностей в БД

---
+++ Services/OrderAPI/DeliveryService.Services.OrderAPI.Infrastructure/Persistence/EntityTypeConfiguration/OrderConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using DeliveryService.Services.OrderAPI.Domain.Order;
+
+namespace DeliveryService.Services.OrderAPI.Infrastructure.Persistence.EntityTypeConfiguration;
+
+public class OrderConfiguration : IEntityTypeConfiguration<OrderEntity>
+{
+	public void Configure(EntityTypeBuilder<OrderEntity> builder)
+	{
+		builder.HasKey(order => order.Id);
+
+		builder.Property(order => order.CustomerId);
+		builder.Property(order => order.CourierId);
+		builder.Property(order => order.ManagerId);
+
+		builder.Property(order => order.Created);
+		builder.Property(order => order.End);
+		builder.Property(order => order.Status);
+
+		builder.Property(order => order.Id)
+		   .IsRequired()
+		   .ValueGeneratedNever();
+
+		builder.HasMany(item => item.OrderItems);
+	}
+}
---
+++ Services/OrderAPI/DeliveryService.Services.OrderAPI.Infrastructure/Persistence/EntityTypeConfiguration/OrderItemConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using DeliveryService.Services.OrderAPI.Domain.OrderItem;
+
+namespace DeliveryService.Services.OrderAPI.Infrastructure.Persistence.EntityTypeConfiguration;
+
+public class OrderItemConfiguration : IEntityTypeConfiguration<OrderItemEntity>
+{
+	public void Configure(EntityTypeBuilder<OrderItemEntity> builder)
+	{
+		builder.HasKey(item => item.Id);
+
+		builder.Property(item => item.Count);
+		builder.Property(item => item.TotalPrice);
+	}
+}
