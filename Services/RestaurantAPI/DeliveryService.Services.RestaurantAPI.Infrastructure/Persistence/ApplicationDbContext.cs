f7994002899926516d7ddcc65b1e237b92724830
pin11kurochkin <pin11kurochkin@gmail.com>
2023-11-05 23:48:02 +0300

    Контекст БД

--- Services/RestaurantAPI/DeliveryService.Services.RestaurantAPI.Infrastructure/Persistence/ApplicationDbContext.cs
+++ Services/RestaurantAPI/DeliveryService.Services.RestaurantAPI.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using DeliveryService.Services.RestaurantAPI.Domain.Manager;
 using DeliveryService.Services.RestaurantAPI.Domain.Restaurant;
+using DeliveryService.Services.RestaurantAPI.Infrastructure.Persistence.EntityTypeConfiguration;
 using Microsoft.EntityFrameworkCore;
 
 namespace DeliveryService.Services.RestaurantAPI.Infrastructure.Persistence;
@@ -11,4 +12,12 @@ public class ApplicationDbContext : DbContext
 
 	public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
 	   : base(options) { }
+
+	protected override void OnModelCreating(ModelBuilder builder)
+	{
+		builder.ApplyConfiguration(new ManagerConfiguration());
+		builder.ApplyConfiguration(new RestaurantConfiguration());
+
+		base.OnModelCreating(builder);
+	}
 }
---
+++ Services/RestaurantAPI/DeliveryService.Services.RestaurantAPI.Infrastructure/Persistence/EntityTypeConfiguration/RestaurantConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using DeliveryService.Services.RestaurantAPI.Domain.Restaurant;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DeliveryService.Services.RestaurantAPI.Infrastructure.Persistence.EntityTypeConfiguration;
+
+public class RestaurantConfiguration : IEntityTypeConfiguration<RestaurantEntity>
+{
+	public void Configure(EntityTypeBuilder<RestaurantEntity> builder)
+	{
+		builder.HasKey(restaraunt => restaraunt.Id);
+		builder.Property(restaraunt => restaraunt.Name);
+		builder.Property(restaraunt => restaraunt.Address);
+		builder.Property(restaraunt => restaraunt.Status);
+	}
+}
