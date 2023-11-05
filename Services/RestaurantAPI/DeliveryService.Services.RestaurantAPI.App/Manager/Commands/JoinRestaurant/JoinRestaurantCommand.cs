feba448a15fe67ae4d5a6e321724a665e54dd71b
pin11kurochkin <pin11kurochkin@gmail.com>
2023-11-05 23:30:15 +0300

    Обработка запроса на привязку менеджера к ресторану

---
+++ Services/RestaurantAPI/DeliveryService.Services.RestaurantAPI.App/Manager/Commands/JoinRestaurant/JoinRestaurantCommand.cs
@@ -0,0 +1,9 @@
+﻿using ErrorOr;
+using MediatR;
+
+namespace DeliveryService.Services.RestaurantAPI.App.Manager.Commands.JoinRestaurant;
+
+public record JoinRestaurantCommand(
+	string ManagerId,
+	string RestaurantId
+	) : IRequest<ErrorOr<bool>>;
