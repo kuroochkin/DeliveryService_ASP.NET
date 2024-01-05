e6e5311f41674fdb20a210484a822fb99c76da67
pin11kurochkin <pin11kurochkin@gmail.com>
2024-01-05 03:31:59 +0300

    Добавлены сущности корзины

---
+++ Services/DeliveryService.Services.BacketAPI.Core/Entities/ShoppingCartItem.cs
@@ -0,0 +1,9 @@
+﻿namespace DeliveryService.Services.BacketAPI.Core.Entities;
+
+public class ShoppingCartItem
+{
+    public string ProductId { get; set; }
+    public string ProductName { get; set; }
+    public int Quantity { get; set; }
+    public decimal Price { get; set; }
+}
